a1f696670923212a65b0dc69341e834f869b0064
ManishArya <manisharya26@gamil.com>
2020-04-24 15:25:55 +0530

    spacing changes

--- src/components/colors/colorRow.jsx
+++ src/components/colors/colorRow.jsx
@@ -4,7 +4,7 @@ export default function ColorRows(props) {
   return (
     <tr>
       <td>{props.id}</td>
-      <td>{props.colorCode}</td>
+      <td>{props.colorCode} </td>
       <td>{props.colorName}</td>
       <td>
         <button onClick={() => props.onEdit(props.id)}>Edit</button> &nbsp;
